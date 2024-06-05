d352ff6007a78a3c52482eb0326055f442e3a037
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-06-05 23:48:53 +0800

    feat: add desc to new keymap
    If we don't supply desc we will only see `lR` which does not make any
    sense.

--- lua/polish.lua
+++ lua/polish.lua
@@ -28,4 +28,4 @@ vim.filetype.add {
 
 vim.o.scrolloff = 10
 -- INFO: i wanna keep `gr` for go to references
-vim.api.nvim_set_keymap("n", "gr", " lR", { nowait = true })
+vim.api.nvim_set_keymap("n", "gr", " lR", { nowait = true, desc = "Search references" })
