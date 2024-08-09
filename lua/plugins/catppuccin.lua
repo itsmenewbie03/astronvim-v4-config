bda957c23bc5030d9dc460060da2ab9efd27dfb8
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-08-09 17:30:06 +0800

    feat: get rid of annoying gray TabLineFill

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,7 +5,10 @@ return {
     -- configuration options...
     transparent_background = true,
     flavour = "mocha",
-    custom_highlights = {},
+    custom_highlights = {
+      -- WARN: who tf added a fucking gray line on my TabLine?
+      TabLineFill = { bg = "#1e1e2e" },
+    },
     integrations = {
       neotree = false,
     },
